1fb9c49049bbfbb5aaf758dd3779f215f2ef9bcd
Cute Newbie <dongyoong5@naver.com>
2023-02-28 01:31:50 +0900

    Add files via upload

--- 2d_array_string_application.c
+++ 2d_array_string_application.c
@@ -1,22 +1,10 @@
-/*다섯 개의 문자열을 입력 받아 char 5행 80얄 2 차원 배열에 순서대로
-저장한 후 문자열 전체,첫 문자,마지막 문자를 추력하는
+/*다섯 개의 문자열을 입력 받아 char 5행 80열 2 차원 배열에 순서대로
+저장한 후 문자열 전체,첫 문자,마지막 문자를 출력하는
 프로그램 작성하기*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void printer(char (*arr)[80]);
-void printer(char (*arr)[80])
-{
-
-
-
-
-
-}
-
-
-
 
 int main(void)
 {
